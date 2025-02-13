71620b2cd99b3f68351904de3dd0b769640c1f0c
TharunthejaB <tharuntheja.b25@gmail.com>
2025-02-14 00:36:08 +0530

    #5 Photo Updation

--- portfolio/src/About.jsx
+++ portfolio/src/About.jsx
@@ -7,10 +7,10 @@ function About() {
             <h2>About</h2>
         </div>
         <div className='flex justify-between w-full items-center'>
-            <div className='w-5/12'>
-                <img src="/src/assets/Rectangle 15.png" alt="" />
+            <div className='xl:w-4/12 h-full'>
+                <img src="/src/assets/T2.png" alt="" className='w-full h-full object-cover' />
             </div>
-            <div className='w-7/12 flex flex-col justify-between gap-9 items-end'>
+            <div className='w-7/12 flex flex-col justify-between xl:gap-4.5 items-end'>
                 <div className='flex flex-col items-end gap-4.5'>
                         <h2>Hello, I'm Tharun theja Boyalla</h2>
                         <p className='text-justify'>I'm a developer, designer, and video editor who loves building intuitive UIs, crafting engaging edits, and creating interactive web apps with React. Currently exploring the MERN stack and always looking to learn something new. When I’m not working, you’ll find me at the gym or catching a good movie.</p>
--- portfolio/src/Herosection.jsx
+++ portfolio/src/Herosection.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Herosection() {
   return (
-    <section className='w-full h-screen flex justify-between items-center -mt-8'>
+    <section className='w-full h-screen flex justify-between items-center xl:-mt-2 2xl:-mt-8'>
       <div className='flex flex-col gap-4.5 w-7/12'>
       <div className='flex justify-between flex-col  gap-3.5'>
         <div>
           <h2>Hi, I'm Tharun theja Boyalla</h2>
-          <h2>a passionate <span>UI/UX Designer</span></h2>
+          <h2>a passionate <span className='text-[#C93D3D]'>Web Developer</span></h2>
         </div>
         <div>
           <p>I create engaging experiences through design, storytelling, and development. Let's bring your ideas to life.</p>
@@ -17,8 +17,8 @@ function Herosection() {
         <button className="rounded-[20px] w-48 h-14 cursor-pointer text-white font-medium bg-green-700">Get in touch</button>
       </div>
       </div>
-      <div className='w-5/12 flex items-center justify-end'>
-      <img src='/src/assets/Rectangle 15.png'></img>
+      <div className='xl:w-4/12 2xl:w-5/12 flex items-center justify-end'>
+      <img src='/src/assets/T1.png' className='w-full'></img>
       </div>
     </section>
   )
