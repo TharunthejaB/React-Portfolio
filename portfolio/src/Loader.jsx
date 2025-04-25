c54d3daea88f9fb4a4df56246b0432afe8964ff2
Tharun theja Boyalla <tharunthejab@cabcsgroup-india.com>
2025-04-25 11:49:26 +0530

    Loader change

--- portfolio/src/Loader.jsx
+++ portfolio/src/Loader.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function Loader() {
   return (
     <div className="w-full h-screen bg-white flex place-content-center place-items-center">
-      <img src="/public/Loader.gif" className="w-3/4 sm:w-1/2 lg:w-1/4"></img>
+      <img
+        src="/src/assets/Loader.gif"
+        className="w-3/4 sm:w-1/2 lg:w-1/4"
+      ></img>
     </div>
   );
 }
--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -6,5 +6,4 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-  base: process.env.VITE_BASE_PATH || "/React-Portfolio/tree/main/portfolio"
 })
