e6fb4eda2c419e84f3be404d328640033256636b
Tharun theja Boyalla <tharunthejab@cabcsgroup-india.com>
2025-04-25 11:38:26 +0530

    Error Change

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,4 @@
 import { Suspense, useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./Navbar";
 import Herosection from "./Herosection";
